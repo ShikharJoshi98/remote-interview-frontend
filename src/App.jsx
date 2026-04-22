d73c90b70b01eb2c5373735c36a8987b96cda1b3
ShikharJoshi98 <shikharjoshi89@gmail.com>
2026-04-22 19:26:02 +0530

    route for landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import DashboardLayout from "./layouts/DashboardLayout";
+import { LandingPage } from "./pages/LandingPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<DashboardLayout />}>
           <Route index element={<Dashboard />} />
         </Route>
