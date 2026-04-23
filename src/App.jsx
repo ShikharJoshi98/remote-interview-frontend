a68cdb29303d94f7f31af703dc2c9605b9ee6c7c
ShikharJoshi98 <shikharjoshi89@gmail.com>
2026-04-23 19:54:29 +0530

    landing pageroutes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import DashboardLayout from "./layouts/DashboardLayout";
 import { LandingPage } from "./pages/LandingPage";
+import { LandingPageLayout } from "./layouts/LandingPageLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<LandingPageLayout />} >
+          <Route index element={<LandingPage />} />
+        </Route>
         <Route path="/dashboard" element={<DashboardLayout />}>
           <Route index element={<Dashboard />} />
         </Route>
